0b295e455b3a74a5b9a9538d90b7b5b1ae97f6e9
Hamish Burke <burkehami@myvuw.ac.nz>
2023-09-10 14:35:36 +1200

    Add Junit to dependencies and add main and test configurations

--- src/main/java/test/nz/ac/wgtn/swen225/lc/domain/DomainTests.java
+++ src/main/java/test/nz/ac/wgtn/swen225/lc/domain/DomainTests.java
@@ -1,4 +1,10 @@
 package test.nz.ac.wgtn.swen225.lc.domain;
 
+import org.junit.Test;
+
 public class DomainTests {
+    @Test
+    public void test() {
+        System.out.println("Hello World!");
+    }
 }
--- src/main/java/test/nz/ac/wgtn/swen225/lc/recorder/RecorderTests.java
+++ src/main/java/test/nz/ac/wgtn/swen225/lc/recorder/RecorderTests.java
@@ -1,4 +1,10 @@
 package test.nz.ac.wgtn.swen225.lc.recorder;
 
+import org.junit.Test;
+
 public class RecorderTests {
+    @Test
+    public void test() {
+        System.out.println("Hello World!");
+    }
 }
