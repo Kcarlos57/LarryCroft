28b3f0f32b670eb0cd929a80fb12c66bc91fff93
strongjona <strongjona@myvuw.ac.nz>
2023-09-14 14:03:52 +1200

    Started implementing Domain Interface

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Domain.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Domain.java
@@ -1,4 +1,26 @@
 package nz.ac.wgtn.swen225.lc.domain;
 
 public class Domain {
+    /**
+     *   public void TryMoveChap(Direction direction)  -   Handles attempting to move chap in the specified direction
+     *
+     *
+     */
+
+    //Public variables
+    public enum  Direction {Up, Down, Left, Right};
+
+
+    /**
+     * Handles attempting to move chap in the specified direction
+     * @param direction
+     */
+    public void TryMoveChap(Direction direction){
+
+    }
+
+
+
+
+
 }
