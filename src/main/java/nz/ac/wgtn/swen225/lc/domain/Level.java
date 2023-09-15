2eff30ed7905fc9588dd35bb1652f3fdad9d260f
strongjona <strongjona@myvuw.ac.nz>
2023-09-15 12:15:48 +1200

    Created layout for Level class, to hold the state of the current level

---
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Level.java
@@ -0,0 +1,14 @@
+package nz.ac.wgtn.swen225.lc.domain;
+import nz.ac.wgtn.swen225.lc.domain.tiles.Tile;
+
+public class Level {
+    //TODO Modify to take from persistency
+    public Level(){
+
+    }
+    Tile[][] tiles;
+    Actor larry;
+    Actor[] actors;
+
+
+}
