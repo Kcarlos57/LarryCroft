4566775cb08fef95dce463017ccdee53ba1cd012
strongjona <strongjona@myvuw.ac.nz>
2023-09-22 15:34:51 +1200

    Basic Actor class is implemented, needs tests and checking but should have relevant classes for not

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Actor.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Actor.java
@@ -1,19 +1,46 @@
 package nz.ac.wgtn.swen225.lc.domain;
 
+import nz.ac.wgtn.swen225.lc.domain.tiles.Tile_Wall;
+
+import java.net.URL;
+
+/**
+ * Controls and mangages non-player controled characters
+ */
+//TODO Add Tests
 public class Actor{
     private Position position;
+    private Domain.Direction directionFacing;   // The direction the actor is facing
 
+    private URL imageReference;
 
-    Actor(){
+    /**
+     * Creates a new actor
+     * @param position
+     * @param directionFacing
+     * @param path
+     */
+    public Actor(Position position, Domain.Direction directionFacing, String path){
+        this.position = position;
+        this.directionFacing = directionFacing;
+
+        try{
+            this.imageReference = Tile_Wall.class.getResource(path);
+        }catch (NullPointerException np) { throw new RuntimeException("Could not find given path for actor"); }
 
     }
 
 
     /**
-     * Moves the actor in the specified direction
+     * Moves the actor in the specified direction, does no checking
      */
     public void Move(Domain.Direction direction){
-
+        switch (direction){
+            case Up -> position.setX(position.getY()-1);
+            case Down -> position.setX(position.getY()+1);
+            case Left -> position.setX(position.getX()-1);
+            case Right -> position.setX(position.getX()+1);
+        }
     }
 
     /**
