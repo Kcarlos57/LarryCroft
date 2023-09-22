7b513f4134a20287381647c24a7cb17ddd18261a
strongjona <strongjona@myvuw.ac.nz>
2023-09-22 15:49:23 +1200

    Setup getters for level class

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Level.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Level.java
@@ -1,14 +1,49 @@
 package nz.ac.wgtn.swen225.lc.domain;
 import nz.ac.wgtn.swen225.lc.domain.tiles.Tile;
 
+/**
+ * Holds the data containing a level
+ */
 public class Level {
+
+
     //TODO Modify to take from persistency
+
+    /**
+     * Creates a level from relevant data
+     */
     public Level(){
 
     }
-    Tile[][] tiles;
-    Actor larry;
-    Actor[] actors;
+    private Tile[][] tiles;
+    private Actor larry;
+    private Actor[] actors;
+
+    int treasureNum;
+
+    /**
+     * Returns a 2d array of tile objects containing the tile data o the level
+     * @return
+     */
+    public Tile[][] getTiles(){
+        return tiles;
+    }
+
+    /**
+     * Gets larry
+     * @return
+     */
+    public Actor getLarry(){
+        return  larry;
+    }
+
+
+    /**
+     * Causes all non-player actors to make a move
+     */
+    public void Step(){
+
+    }
 
 
 }
