ed48d0e2d239f7b512db2bbd2f82ddcf5ec9f25a
strongjona <strongjona@myvuw.ac.nz>
2023-09-22 15:42:05 +1200

    Added integration with the level class, need to inergrate with persistancy, need tests

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Domain.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Domain.java
@@ -14,10 +14,17 @@ public class Domain {
      *
      */
 
-    private Persistency persistency;
-    public Domain(){
 
-    }
+
+    public enum  Direction {Up, Down, Left, Right};
+
+
+
+    private Persistency persistency = null;
+
+    private Level currentLevel = null;
+
+
 
     /**
      * Sets the references between modules
@@ -27,15 +34,52 @@ public class Domain {
         this.persistency = persistency;
     }
 
+    /**
+     * Handles Setting up the domain class variables, SetModuleLinksShould be called before this or it will fail
+     */
+    public void Setup(){
+        if(persistency==null) throw new IllegalStateException("Domain cannot access Persistency class");
+
+        //TODO Cannot impement properly until the format of the persistency class is created, temp for now so I can keep working
+
+        String defaultLevel = "Level1";
+        LoadLevel(defaultLevel);
 
 
+    }
+
+    /**
+     * Loads a new Level into the Domain, removes all traces of prevoiusly loaded level if any, call SaveLevel first if you want to save previous
+     * @param levelName //TODO do not know what final format is for this, guessing string though
+     */
+    public void LoadLevel(String levelName){
+        //TODO Needs Persistency
+
+        //Level newLevel = ConvertToLevel(persistency.LoadLevel(levelName)); //Need to do sometinh here
+        Level newLevel = null;
+
+        if(newLevel == null) throw new IllegalStateException("The level being loaded is null");
+        currentLevel = newLevel;
+    }
+
+    /**
+     *  Saves the current level with all actors in their current place and state
+     */
+    public void SaveCurrentLevel(){
+        //TODO Needs Persistency
+        //persistency.Save(currentLevel.toPersistencyFormat());
+    }
 
 
-    //Public variables
-    public enum  Direction {Up, Down, Left, Right};
+    /**
+     * Gets the current level
+     * @return
+     */
+    public Level getCurrentLevel(){
+        return currentLevel;
+    }
 
 
-    //Private
 
 
 
@@ -48,9 +92,13 @@ public class Domain {
     }
 
 
-    public void Setup(){
 
-    }
+
+
+
+
+
+
 
 
 
