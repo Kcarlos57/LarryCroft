5ad575720fa76e25e8039396fcd6cbe0f1f8ac4b
strongjona <strongjona@myvuw.ac.nz>
2023-09-25 12:38:45 +1300

    Added equals method to key

---
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/tiles/Tile_LockedDoor.java
@@ -0,0 +1,42 @@
+package nz.ac.wgtn.swen225.lc.domain.tiles;
+
+import nz.ac.wgtn.swen225.lc.domain.items.Item;
+
+import java.net.URL;
+
+public class Tile_LockedDoor implements Tile{
+    @Override
+    public boolean isWalkable() {
+        return false;
+    }
+
+    @Override
+    public URL getTileImageReference() {
+        return getResource("/Tiles/WallTile.png");
+    }
+
+    @Override
+    public String toString() {
+        return "#";
+    }
+
+    @Override
+    public boolean hasItem(){
+        return false;
+    }
+
+    @Override
+    public Item getItem(){
+        throw new IllegalStateException("This Tile cannot contain Items");
+    }
+
+    @Override
+    public void SetItem(Item item) {
+        throw new IllegalStateException("This Tile cannot contain Items");
+    }
+
+    @Override
+    public Item RemoveItem() {
+        throw new IllegalStateException("This Tile cannot contain Items");
+    }
+}
